bc2952d2ff617d4b1a6a70704578bc482156a2de
L4R4Y <sebaxtian.larahome@hotmail.com>
2026-09-14 22:42:19 -0500

    conexion base de datos y ModeloCurso

--- clases/conexion.php
+++ clases/conexion.php
@@ -1,1 +1,31 @@
 <?php
+
+class Conexion{
+
+    private string $host = 'localhost';
+    private string $usuario = 'root';
+    private string $password = '123456789';
+    private string $baseDatos = 'magiscode';
+
+    private PDO $conexion;
+
+    public function __construct(){
+        $this->conexion = new PDO(
+            "mysql:host={$this->host};dbname={$this->baseDatos}; charset=utf8",
+            $this->usuario,
+            $this->password
+        );
+
+        $this->conexion->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION);
+
+    }
+
+    public function getConexion(): PDO{
+        return $this->conexion;
+    }
+
+
+
+}
---
+++ clases/prueba_conexion.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'conexion.php';
+
+$conexion = new Conexion();
+
+echo 'Conexion realizada correctamente';
